11f5512fc04a3d6d38565536b6e122ce6215a8c4
Shubh0777 <104527159+Shubh0777@users.noreply.github.com>
2026-04-28 01:43:14 +0530

    Refactor toggleDetails function for brevity

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ function toggleDetails(cardId) {
     const card = document.getElementById(cardId);
     const body = card.querySelector('.card-body');
 
-    if (body.style.display === "block") {
-        body.style.display = "none";
-    } else {
-        body.style.display = "block";
-    }
+    body.style.display = body.style.display === "block" ? "none" : "block";
 }
